6e70533050d0c15f9bee507b594b455ceefafd96
elinordin <elin.evelina@live.se>
2021-08-24 16:21:55 +0200

    display correct information, add basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { useContext, useEffect } from 'react'
 import './App.css'
-import {Store} from './Store'
+import { Store } from './Store'
 
-function App():JSX.Element {
-  const {episodes, dispatch} = useContext(Store)
+function App(): JSX.Element {
+  const { episodes, dispatch } = useContext(Store)
 
   const fetchData = async () => {
-    const data = await fetch('https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes').then(data => data.json())
-    return dispatch({type: 'FETCH_DATA', payload: data._embedded.episodes})
+    const data = await fetch('https://api.tvmaze.com/singlesearch/shows?q=mirror&embed=episodes').then(data => data.json())
+    return dispatch({ type: 'FETCH_DATA', payload: data._embedded.episodes })
   }
 
   useEffect(() => {
@@ -16,9 +16,20 @@ function App():JSX.Element {
 
   return (
     <>
-          <h1>Rick and Morty</h1>
-          <p>Pick your favourite episode</p>
-          {console.log(episodes)}
+      <h1>Black mirror</h1>
+      <p>Pick your favourite episode</p>
+      <ul className='episode-grid'>
+        {episodes.all.map((episode:any) => (
+          <li key={episode.id}>
+            <img src={episode.image.medium} alt={`Black mirror episode ${episode.name}`} />
+            <ul>
+              <li>{episode.name}</li>
+              <li>Season: {episode.season}</li>
+              <li>Number: {episode.number}</li>
+            </ul>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
